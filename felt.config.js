35e595950e64017cf5958e7f343a5664fb02d894
Shawn Allen <shawn.allen@gsa.gov>
2016-08-10 16:55:40 -0700

    first commit

---
+++ felt.config.js
@@ -0,0 +1,49 @@
+'use strict'
+
+/**
+ * Standard recipe for Felt
+ */
+
+const rollup = require('felt-rollup');
+const babel = require('rollup-plugin-babel');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const postcss = require('felt-postcss');
+const postcssImport = require('postcss-import');
+const cssnext = require('postcss-cssnext');
+const sass = require('felt-sass');
+
+const post = postcss({
+  plugins: [
+    postcssImport(),
+    cssnext()
+  ],
+  options: {
+    map: { inline: false }
+  }
+});
+
+module.exports = {
+  // default handlers for each extension
+  handlers: {
+    '.js': rollup({
+      plugins: [
+        resolve({
+          jsnext: true,
+          main: true,
+          browser: true
+        }),
+        commonjs(),
+        babel({
+          babelrc: false,
+          presets: ['es2015-rollup']
+        })
+      ],
+      sourceMap: true
+    }),
+    '.scss': sass({
+      post: post
+    }),
+    '.css': post
+  }
+};
