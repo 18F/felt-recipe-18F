07113f22ae557d53aca5493411e5cc56f67106bc
Shawn Allen <shawn.allen@gsa.gov>
2016-08-24 11:05:23 -0700

    uglify JS

--- felt.config.js
+++ felt.config.js
@@ -8,6 +8,8 @@ const rollup = require('felt-rollup');
 const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
+const uglify = require('rollup-plugin-uglify');
+
 const postcss = require('felt-postcss');
 const postcssImport = require('postcss-import');
 const cssnext = require('postcss-cssnext');
@@ -37,6 +39,8 @@ module.exports = {
         babel({
           babelrc: false,
           presets: ['es2015-rollup']
+        }),
+        uglify({
         })
       ],
       sourceMap: true
